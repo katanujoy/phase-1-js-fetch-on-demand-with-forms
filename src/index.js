4a7d3be2e4943ab2c2d6b874d616d21c05326afd
katanujoy <katanujoyy99@gmail.com>
2025-03-19 16:11:55 +0300

    Completed Phase 1 Fetch on Demand assignment with form submission handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
-}
+fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Movie not found!");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+    })
+    .catch((error) => {
+        console.log("Error:", error);
+        alert("Movie not found. Please enter a valid ID.");
+    });
